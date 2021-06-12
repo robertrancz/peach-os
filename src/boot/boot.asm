33a6c5cc4831693ce2a8b93bdbed221f3b194be9
Robert Rancz <rancz.robi@gmail.com>
2021-06-12 22:07:16 +0300

    Create and load 32 bit kernel into memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -62,23 +62,74 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1  ; size of the descriptor
     dd gdt_start                ; offset
 
+; Load the kernel into memory and jump to it
 [BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
+    mov eax, 1          ; starting sector is 1 because 0 is the boot sector
+    mov ecx, 100        ; total no of sectors we want to load
+    mov edi, 0x0100000  ; the address to load into
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
 
-    ; Enable the A20 line. For details see https://wiki.osdev.org/A20_Line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
+; Dummy driver to get the kernel loaded
+ata_lba_read:
+    mov ebx, eax        ; Backup the LBA
+    ; Send the highest 8 bits of the LBA to hard disk controller
+    shr eax, 24
+    or eax, 0xE0        ; Select the master drive
+    mov dx, 0x1F6
+    out dx, al
+    ; Finished sending highest 8 bits to the LBA
+    
+    ; Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
+    ; Finished sending the total sectors to read
 
-    jmp $
+    ; Send more bits of the LBA
+    mov eax, ebx        ; Restore the backup LBA
+    mov dx, 0x1F3
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send more bits of the LBA
+    mov dx, 0x1F4
+    mov eax, ebx        ; Restore the backup LBA
+    shr eax, 8
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send upper 16 bits of the LBA
+    mov dx, 0x1F5
+    mov eax, ebx        ; Restore the backup LBA
+    shr eax, 16
+    out dx, al
+    ; Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1F7
+    mov al, 0x20
+    out dx, al
+
+; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1F7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+    ; We need to read 256 words at a time (256 words = 512 bytes = 1 sector)
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw        ; https://faydoc.tripod.com/cpu/insw.htm
+    pop ecx
+    loop .next_sector
+    ; End of reading sectors into memory
+    ret
 
 times 510-($ - $$) db 0     ; fill at least 510 bytes of data
 dw 0xAA55       ; little endian (low-order byte is stored on the starting address) -> this will be 0x55AA
