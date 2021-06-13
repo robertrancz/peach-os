970df7efe0ac9d77698d8d7f170fa01171bd306e
Robert Rancz <rancz.robi@gmail.com>
2021-06-13 11:51:20 +0300

    Implement print to terminal

--- src/kernel.c
+++ src/kernel.c
@@ -1,24 +1,72 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
 uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
 uint16_t terminal_make_char(char c, char colour)
 {
     return (colour << 8) | c;
 }
 
+void terminal_put_char(int x, int y, char c, char colour)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
+
+void terminal_write_char(char c, char colour)
+{
+    if(c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
+    }
+
+    terminal_put_char(terminal_col, terminal_row, c, colour);
+    terminal_col += 1;
+    if(terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+
 void terminal_initialize()
 {
     video_mem = (uint16_t*)(0xB8000);
+    terminal_col = 0;
+    terminal_row = 0;
 
     // set a blank black display
     for (int y = 0; y < VGA_HEIGHT; y++)
     {
         for (int x = 0; x < VGA_WIDTH; x++)
         {
-            video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(' ', 0);
+            terminal_put_char(x, y, ' ', 0);
         }
+    }
+}
+
+size_t strlen(const char* str)
+{
+    size_t len = 0;
+    while (str[len])
+    {
+        len++;
+    }
+    
+    return len;
+}
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_write_char(str[i], 15);
     }
 }
 
@@ -26,12 +74,5 @@ void kernel_main()
 {
     terminal_initialize();
 
-    // Put a white B on screen
-    video_mem[0] = terminal_make_char('B', 15);
-
-    /*
-    char* video_mem = (char*)(0xB8000);
-    video_mem[0] = 'A';     // code byte
-    video_mem[1] = 2;       // attribute byte (2 = green)
-    */
+    print("Hello world!\n   --Robert\n ");
 }
